a3968371a532cecdd31bc762daaf75bee709b998
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2024-02-23 00:04:54 +0530

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,7 @@ async def send_message(message, user_message, is_private):
 
 def run_discord_bot():
     TOKEN = '<user_token>'
-    intents = discord.Intents.default()
+    intents = discord.Intents.all()
     intents.message_content = True
     client = discord.Client(intents=intents)
 
